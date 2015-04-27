6157b042042948f3b2eb83717939997ca8fd641b
tro3 <trey.roessig@gmail.com>
2015-04-26 19:06:20 -0700

    Clearing out previous...

--- build/src/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('appTop', ['templates-app', 'templates-common', 'app.home', 'app.userform', 'app.groupform', 'ui.router']).config(function($stateProvider, $urlRouterProvider) {
-  return $urlRouterProvider.otherwise('/home');
-}).run(function() {}).controller('appCtrl', function($scope, $location) {
-  return $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-    if (angular.isDefined(toState.data.pageTitle)) {
-      return $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
-    }
-  });
-});
